c3206c6c2d9a582eae7e38c54296ef74e89f39de
Conail Stewart <conail@conail.net>
2015-06-17 20:45:13 -0400

    Add sketch for user tests.

---
+++ spec/userSpec.js
@@ -0,0 +1,10 @@
+/**
+ * Users can register.
+ * Users can recieve messages via email.
+ * User can view their own profile (image).
+ * User can view course list.
+ * Timed messages can be sent according to CommsSchedule (single transmission after event).
+ * User can view sent messages as HTML.
+ * PDF document can be downloaded after an event.
+ **/
+
